d5a03fcf5367148210c9adba342a0450a2236a9e
gaming guru <gaming.guru24x7@gmail.com>
2016-09-01 23:54:52 +0500

    my initial commit

---
+++ src/app.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+request = requests.get("http://www.officedepot.com/a/products/510830/WorkPro-Quantum-9000-Series-Ergonomic-Mesh/")
+content = request.content
+# <span class="pricing_format fleft" data-auid="productDetail_value_itemPrice0">289</span>
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find("span", {"class": "pricing_format fleft", "data-auid": "productDetail_value_itemPrice0"})
+string_price = element.text.strip()
+price_without_symbol = int(string_price[0:])
+
+if price_without_symbol < 290:
+    print("You should buy the chair")
+    print("The current price is {} ".format(string_price))
+else:
+    print("Do not buy, its to costly")
+
+
+
+
+
+
+
+
+
